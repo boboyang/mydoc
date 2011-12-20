6f384bb1d8032a6d4fc27b7ddff42523bbaaf7e9
whille <Whille_Wang@cn.dlink.com>
2011-12-20 18:28:58 +0800

    modify

--- python.mm
+++ python.mm
@@ -1,6 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#000000" CREATED="1303868524495" ID="ID_1852631055" MODIFIED="1303869571394" TEXT="python">
+<node COLOR="#000000" CREATED="1303868524495" ID="ID_1852631055" MODIFIED="1324366165741" TEXT="python">
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
 <node COLOR="#0033ff" CREATED="1303868531244" ID="ID_740578310" MODIFIED="1303869571394" POSITION="left" TEXT="install">
@@ -9,7 +9,9 @@
 <node COLOR="#00b439" CREATED="1303868534025" ID="ID_1010803028" MODIFIED="1303869571394" TEXT="windows">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303868536869" ID="ID_1144772391" LINK=" http://www.activestate.com/Products/ActivePython" MODIFIED="1303869571394" TEXT="ActivePython"/>
+<node COLOR="#990000" CREATED="1303868536869" ID="ID_1144772391" LINK=" http://www.activestate.com/Products/ActivePython" MODIFIED="1324366161647" TEXT="ActivePython">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 <node COLOR="#0033ff" CREATED="1303868610487" ID="ID_185760260" MODIFIED="1303869571394" POSITION="left" TEXT="books">
@@ -18,9 +20,15 @@
 <node COLOR="#00b439" CREATED="1303868614455" ID="ID_600162389" LINK="http://woodpecker.org.cn/diveintopython/" MODIFIED="1303869571394" TEXT="Dive into pytyon">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303872724795" ID="ID_1945671805" MODIFIED="1303872756292" TEXT="quick for c++/java developer"/>
-<node COLOR="#ff00ff" CREATED="1303872759885" ID="ID_397393237" MODIFIED="1303872808193" TEXT="convinient ebook and src examples"/>
-<node COLOR="#ff00ff" CREATED="1303868650326" ID="ID_706407290" LINK="D:\book\diveintopythonzh-cn-5.4b\html\diveintopythonzh-cn.html" MODIFIED="1303874592834" TEXT="local doc"/>
+<node COLOR="#990000" CREATED="1303872724795" ID="ID_1945671805" MODIFIED="1324366161650" TEXT="quick for c++/java developer">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303872759885" ID="ID_397393237" MODIFIED="1324366161651" TEXT="convinient ebook and src examples">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303868650326" ID="ID_706407290" LINK="D:\book\diveintopythonzh-cn-5.4b\html\diveintopythonzh-cn.html" MODIFIED="1324366161651" TEXT="local doc">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 <node COLOR="#0033ff" CREATED="1303868837496" ID="ID_1480915298" MODIFIED="1303869571394" POSITION="right" TEXT="function">
@@ -33,7 +41,9 @@
 <node COLOR="#00b439" CREATED="1303868981529" ID="ID_1202174858" MODIFIED="1303869571394" TEXT="doc string">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303869039117" ID="ID_231792202" MODIFIED="1303869571394" TEXT="&quot;&quot;&quot;"/>
+<node COLOR="#990000" CREATED="1303869039117" ID="ID_231792202" MODIFIED="1324366161655" TEXT="&quot;&quot;&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 <node COLOR="#0033ff" CREATED="1303869857429" ID="ID_1309579113" MODIFIED="1303871142431" POSITION="right" TEXT="data type">
@@ -42,14 +52,29 @@
 <node COLOR="#00b439" CREATED="1303869863054" ID="ID_551188594" MODIFIED="1303884819555" TEXT="dictionary">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303884900141" ID="ID_1695370707" MODIFIED="1303884904235" TEXT="help(dict)"/>
-<node COLOR="#ff00ff" CREATED="1303869870194" ID="ID_1813758011" MODIFIED="1303884814196" TEXT="same as hash map"/>
-<node COLOR="#ff00ff" CREATED="1303869897051" ID="ID_474070209" MODIFIED="1303869897597" TEXT="d = {&quot;server&quot;:&quot;mpilgrim&quot;, &quot;database&quot;:&quot;master&quot;}"/>
-<node COLOR="#ff00ff" CREATED="1303870256266" ID="ID_724683235" MODIFIED="1303870261578" TEXT="d[&quot;server&quot;]"/>
-<node COLOR="#ff00ff" CREATED="1303870435436" ID="ID_346751904" MODIFIED="1303870437733" TEXT=".clear()"/>
-<node COLOR="#ff00ff" CREATED="1303871861424" ID="ID_713133842" MODIFIED="1303871875126" TEXT=".keys()"/>
-<node COLOR="#ff00ff" CREATED="1303871863018" ID="ID_1951962374" MODIFIED="1303871866080" TEXT=".values()"/>
-<node COLOR="#ff00ff" CREATED="1303871866377" ID="ID_1055148200" MODIFIED="1303871870361" TEXT=".items()">
+<node COLOR="#990000" CREATED="1303884900141" ID="ID_1695370707" MODIFIED="1324366161659" TEXT="help(dict)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303869870194" ID="ID_1813758011" MODIFIED="1324366161660" TEXT="same as hash map">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303869897051" ID="ID_474070209" MODIFIED="1324366161661" TEXT="d = {&quot;server&quot;:&quot;mpilgrim&quot;, &quot;database&quot;:&quot;master&quot;}">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870256266" ID="ID_724683235" MODIFIED="1324366161661" TEXT="d[&quot;server&quot;]">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870435436" ID="ID_346751904" MODIFIED="1324366161662" TEXT=".clear()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303871861424" ID="ID_713133842" MODIFIED="1324366161662" TEXT=".keys()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303871863018" ID="ID_1951962374" MODIFIED="1324366161663" TEXT=".values()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303871866377" ID="ID_1055148200" MODIFIED="1324366161664" TEXT=".items()">
+<font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1303872018113" ID="ID_1260572397" MODIFIED="1303872019253" TEXT=" [&quot;%s=%s&quot; % (k, v) for k, v in params.items()]">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
@@ -58,31 +83,63 @@
 <node COLOR="#00b439" CREATED="1303869874225" ID="ID_1305167574" MODIFIED="1303884908531" TEXT="list">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303884908531" ID="ID_1045449598" MODIFIED="1303884911875" TEXT="help(list)"/>
-<node COLOR="#ff00ff" CREATED="1303870469824" ID="ID_396048758" MODIFIED="1303870470527" TEXT="li = [&quot;a&quot;, &quot;b&quot;, &quot;mpilgrim&quot;, &quot;z&quot;, &quot;example&quot;]"/>
-<node COLOR="#ff00ff" CREATED="1303870471105" ID="ID_1587035215" MODIFIED="1303870477932" TEXT="li[0]"/>
-<node COLOR="#ff00ff" CREATED="1303870511742" ID="ID_308815877" MODIFIED="1303870516522" TEXT="li[1:3]"/>
-<node COLOR="#ff00ff" CREATED="1303870722299" ID="ID_1778955347" MODIFIED="1303870726362" TEXT="li[-1]"/>
-<node COLOR="#ff00ff" CREATED="1303870545551" ID="ID_649641365" MODIFIED="1303870547785" TEXT=".append()"/>
-<node COLOR="#ff00ff" CREATED="1303870548332" ID="ID_1595927430" MODIFIED="1303870551128" TEXT=".insert()"/>
-<node COLOR="#ff00ff" CREATED="1303870551769" ID="ID_344247145" MODIFIED="1303870557409" TEXT=".extend()"/>
-<node COLOR="#ff00ff" CREATED="1303870607295" ID="ID_1734701021" MODIFIED="1303870609217" TEXT=".remove()"/>
-<node COLOR="#ff00ff" CREATED="1303870591874" ID="ID_1423739262" MODIFIED="1303870596546" TEXT="len(list)"/>
+<node COLOR="#990000" CREATED="1303884908531" ID="ID_1045449598" MODIFIED="1324366161666" TEXT="help(list)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870469824" ID="ID_396048758" MODIFIED="1324366161666" TEXT="li = [&quot;a&quot;, &quot;b&quot;, &quot;mpilgrim&quot;, &quot;z&quot;, &quot;example&quot;]">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870471105" ID="ID_1587035215" MODIFIED="1324366161667" TEXT="li[0]">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870511742" ID="ID_308815877" MODIFIED="1324366161668" TEXT="li[1:3]">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870722299" ID="ID_1778955347" MODIFIED="1324366161668" TEXT="li[-1]">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870545551" ID="ID_649641365" MODIFIED="1324366161669" TEXT=".append()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870548332" ID="ID_1595927430" MODIFIED="1324366161669" TEXT=".insert()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870551769" ID="ID_344247145" MODIFIED="1324366161670" TEXT=".extend()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870607295" ID="ID_1734701021" MODIFIED="1324366161671" TEXT=".remove()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870591874" ID="ID_1423739262" MODIFIED="1324366161671" TEXT="len(list)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 <node COLOR="#00b439" CREATED="1303870645557" FOLDED="true" ID="ID_99456594" MODIFIED="1303884995945" TEXT="tuple">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303884949980" ID="ID_1062295291" MODIFIED="1303884950840" TEXT="help(tuple)"/>
-<node COLOR="#ff00ff" CREATED="1303870898017" ID="ID_623077878" MODIFIED="1303870914281" TEXT="const, safe and faster than list"/>
-<node COLOR="#ff00ff" CREATED="1303870695208" ID="ID_1802156483" MODIFIED="1303870695833" TEXT="t = (&quot;a&quot;, &quot;b&quot;, &quot;mpilgrim&quot;, &quot;z&quot;, &quot;example&quot;)"/>
-<node COLOR="#ff00ff" CREATED="1303870821821" ID="ID_1452007011" MODIFIED="1303870824055" TEXT=".append()"/>
-<node COLOR="#ff00ff" CREATED="1303870824665" ID="ID_186264962" MODIFIED="1303870826883" TEXT=".remove()"/>
-<node COLOR="#ff00ff" CREATED="1303870827242" ID="ID_784059610" MODIFIED="1303870841038" TEXT=".indiex(&apos;mpilgrim&apos;)"/>
+<node COLOR="#990000" CREATED="1303884949980" ID="ID_1062295291" MODIFIED="1324366161673" TEXT="help(tuple)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870898017" ID="ID_623077878" MODIFIED="1324366161673" TEXT="const, safe and faster than list">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870695208" ID="ID_1802156483" MODIFIED="1324366161674" TEXT="t = (&quot;a&quot;, &quot;b&quot;, &quot;mpilgrim&quot;, &quot;z&quot;, &quot;example&quot;)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870821821" ID="ID_1452007011" MODIFIED="1324366161675" TEXT=".append()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870824665" ID="ID_186264962" MODIFIED="1324366161675" TEXT=".remove()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303870827242" ID="ID_784059610" MODIFIED="1324366161676" TEXT=".indiex(&apos;mpilgrim&apos;)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 <node COLOR="#00b439" CREATED="1303871219017" ID="ID_1606857589" MODIFIED="1303884959683" TEXT="enum">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303871227110" ID="ID_1119165670" MODIFIED="1303884978618">
+<node COLOR="#990000" CREATED="1303871227110" ID="ID_1119165670" MODIFIED="1324366161677">
 <richcontent TYPE="NODE"><html>
   <head>
 
@@ -92,8 +149,7 @@
       (MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY) = range(7)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
 </node>
@@ -101,7 +157,7 @@
 <node COLOR="#00b439" CREATED="1303872429354" ID="ID_1975488805" MODIFIED="1303872514034" TEXT="string">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303871575436" ID="ID_900777103" MODIFIED="1303872514034" TEXT="Formatting Strings">
+<node COLOR="#990000" CREATED="1303871575436" ID="ID_900777103" MODIFIED="1324366161679" TEXT="Formatting Strings">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1303871577670" ID="ID_1105675619" MODIFIED="1303872514034" TEXT="&quot;%s=%d&quot; %(&quot;name&quot;,31)">
@@ -113,11 +169,11 @@
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
-<node COLOR="#ff00ff" CREATED="1303872298711" ID="ID_649887793" MODIFIED="1303872540391" TEXT="s=&apos;;&apos;.join(words)">
+<node COLOR="#990000" CREATED="1303872298711" ID="ID_649887793" MODIFIED="1324366161680" TEXT="s=&apos;;&apos;.join(words)">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#ff00ff" CREATED="1303872392186" ID="ID_757592861" MODIFIED="1303872548812" TEXT="s.split(&apos;;&apos;)">
+<node COLOR="#990000" CREATED="1303872392186" ID="ID_757592861" MODIFIED="1324366161681" TEXT="s.split(&apos;;&apos;)">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1303885498959" ID="ID_1459614076" MODIFIED="1303885513223" TEXT="&quot; &quot;.join(s.split())">
@@ -144,8 +200,12 @@
 <node COLOR="#00b439" CREATED="1303869493605" ID="ID_1907824898" MODIFIED="1303869571394" TEXT="sys.path">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303869508666" ID="ID_460201889" MODIFIED="1303869571394" TEXT="append()"/>
-<node COLOR="#ff00ff" CREATED="1303869539991" ID="ID_520028533" MODIFIED="1303869571394" TEXT="insert()"/>
+<node COLOR="#990000" CREATED="1303869508666" ID="ID_460201889" MODIFIED="1324366161688" TEXT="append()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303869539991" ID="ID_520028533" MODIFIED="1324366161689" TEXT="insert()">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 <node COLOR="#0033ff" CREATED="1303882587145" ID="ID_112853880" MODIFIED="1303883044398" POSITION="right" TEXT="__builtin__">
@@ -154,8 +214,12 @@
 <node COLOR="#00b439" CREATED="1303869435970" ID="ID_1908103255" MODIFIED="1303883906487" TEXT="help(object)">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303884595046" ID="ID_584543104" MODIFIED="1303884595749" TEXT="help(&quot;__builtin__&quot;)"/>
-<node COLOR="#ff00ff" CREATED="1303887799410" ID="ID_1265333829" MODIFIED="1303887805441" TEXT="help(&quot;pass&quot;)"/>
+<node COLOR="#990000" CREATED="1303884595046" ID="ID_584543104" MODIFIED="1324366161692" TEXT="help(&quot;__builtin__&quot;)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303887799410" ID="ID_1265333829" MODIFIED="1324366161693" TEXT="help(&quot;pass&quot;)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 <node COLOR="#00b439" CREATED="1303882592254" ID="ID_1399095798" MODIFIED="1303882595738" TEXT="type()">
 <edge STYLE="bezier" WIDTH="thin"/>
@@ -176,8 +240,12 @@
 <node COLOR="#00b439" CREATED="1303883231192" ID="ID_999097227" MODIFIED="1303884259109" TEXT="getattr()">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303884307214" ID="ID_873323911" MODIFIED="1303884323509" TEXT="Get a named attribute from an object"/>
-<node COLOR="#ff00ff" CREATED="1303884308479" ID="ID_452406078" MODIFIED="1303884343664" TEXT="kernel of introspection"/>
+<node COLOR="#990000" CREATED="1303884307214" ID="ID_873323911" MODIFIED="1324366161699" TEXT="Get a named attribute from an object">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1303884308479" ID="ID_452406078" MODIFIED="1324366161700" TEXT="kernel of introspection">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 <node COLOR="#00b439" CREATED="1303885298884" ID="ID_1888230007" MODIFIED="1303885305540" TEXT="lambda function">
 <edge STYLE="bezier" WIDTH="thin"/>
@@ -194,7 +262,9 @@
 <node COLOR="#00b439" CREATED="1303887608459" ID="ID_647292846" MODIFIED="1303887616177" TEXT="from module import *">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#ff00ff" CREATED="1303887644034" ID="ID_307553247" MODIFIED="1303887751539" TEXT="attributes imported to local namespace"/>
+<node COLOR="#990000" CREATED="1303887644034" ID="ID_307553247" MODIFIED="1324366161706" TEXT="attributes imported to local namespace">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 </node>
