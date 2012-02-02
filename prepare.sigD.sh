0a62459b27f7fc08175b6a9805e4c308587876cb
whille <whille@whille-EP35-DS3L.(none)>
2012-02-03 00:32:50 +0800

    update

--- prepare.sigD.sh
+++ prepare.sigD.sh
@@ -10,6 +10,11 @@ tar zxvf google-perftools-1.9.1.tar.gz
 cd google-perftools-1.9.1/
 ./configure && make && sudo make install
 
+connection detected:
+conn->refcnt = 1;
 
+connection closed:
+if (conn->refcnt == 0)
 
-
+user update firmware:
+upgrade_cmd_type = TRUE;
