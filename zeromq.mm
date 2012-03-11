74793635ca9b5447cb981c9aed3f52203eb9f616
whille <whille@163.com>
2012-03-12 07:28:26 +0800

    update

---
+++ zeromq.mm
@@ -0,0 +1,124 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1331507846606" ID="ID_275746845" MODIFIED="1331508450068" TEXT="zeromq">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1331508239343" ID="ID_1191829356" MODIFIED="1331508242356" POSITION="left" TEXT="types">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Ubuntu" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1331508242359" ID="ID_1173070001" MODIFIED="1331508246275" TEXT="req/rep">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Ubuntu" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1331508246573" ID="ID_1909203148" MODIFIED="1331508249681" TEXT="pub/sub">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Ubuntu" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1331508250108" ID="ID_652470014" MODIFIED="1331508254298" TEXT="pull/push">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Ubuntu" SIZE="16"/>
+<node COLOR="#990000" CREATED="1331508255163" ID="ID_1659106689" MODIFIED="1331508260718" TEXT="streaming">
+<font NAME="Ubuntu" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1331508261034" ID="ID_83212257" MODIFIED="1331508266890" TEXT="load balance">
+<font NAME="Ubuntu" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1331508268424" ID="ID_930458274" MODIFIED="1331508271930" TEXT="pair">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Ubuntu" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1331508005960" ID="ID_875753569" MODIFIED="1331508014295" POSITION="right" TEXT="scalable">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1331508015430" ID="ID_1025271075" MODIFIED="1331508021657" TEXT="pub-sub proxy">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1331508022677" ID="ID_971527426" MODIFIED="1331508035638" TEXT="multipart save">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1331508062190" ID="ID_1775008510" MODIFIED="1331508067358" TEXT="SND_MORE">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1331508038946" ID="ID_1377478128" MODIFIED="1331508059921" TEXT="sub nodes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1331508073675" ID="ID_1805358890" MODIFIED="1331508081034" TEXT="built-in device">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1331508082314" ID="ID_342980411" MODIFIED="1331508085610" TEXT="Queue">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1331508086297" ID="ID_1763844631" MODIFIED="1331508089866" TEXT="Forwarder">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1331508090153" ID="ID_1567815245" MODIFIED="1331508098363" TEXT="Streamer">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1331508315296" ID="ID_913049219" MODIFIED="1331508317938" TEXT="broker">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Ubuntu" SIZE="16"/>
+<node COLOR="#990000" CREATED="1331508318671" ID="ID_345429585" MODIFIED="1331508351118" TEXT="router/dealer">
+<font NAME="Ubuntu" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1331508332174" ID="ID_110340847" MODIFIED="1331508346396" TEXT="net I/O bottleneck problem">
+<font NAME="Ubuntu" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1331508369526" ID="ID_668474964" MODIFIED="1331508377384" POSITION="right" TEXT="multi-threading">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Ubuntu" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1331508403265" ID="ID_1782394302" MODIFIED="1331508423116" TEXT="only context is thread safe">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Ubuntu" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1331508384499" ID="ID_1222200297" MODIFIED="1331508390902" TEXT="signaling">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Ubuntu" SIZE="16"/>
+<node COLOR="#990000" CREATED="1331508391730" ID="ID_1297571721" MODIFIED="1331508396130" TEXT="pair">
+<font NAME="Ubuntu" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1331508419038" ID="ID_288445362" MODIFIED="1331508421808" TEXT="inproc">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Ubuntu" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1331507909985" ID="ID_1712365118" MODIFIED="1331507995427" POSITION="left" TEXT="project">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1331507927223" ID="ID_416784484" MODIFIED="1331507995430" TEXT="error handling">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1331507932329" ID="ID_1406239270" MODIFIED="1331507995432" TEXT="interruption">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1331507938484" ID="ID_490760988" MODIFIED="1331507995434" TEXT="memory leak">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1331507943699" ID="ID_2107057" MODIFIED="1331507995435" TEXT="using valgrind">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1331508451064" ID="ID_873279756" MODIFIED="1331508459962" POSITION="right" TEXT="node coordination">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Ubuntu" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1331508461670" ID="ID_1680563371" MODIFIED="1331508465766" TEXT="pub/sub">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Ubuntu" SIZE="16"/>
+<node COLOR="#990000" CREATED="1331508466997" ID="ID_112859656" MODIFIED="1331508474790" TEXT="wait for all sub ready">
+<font NAME="Ubuntu" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
