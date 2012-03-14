974ce5680cbc61d0ba304750cdc381c7f7160e20
whille <whille@163.com>
2012-03-14 12:16:12 +0800

    update

--- linux/mkusb.sh
+++ linux/mkusb.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /mnt/cdrom
-sudo mount -o loop,ro -t iso9660 ~/Downloads/gentoo/install-amd64-minimal-20120223.iso /mnt/cdrom
+sudo mount -o loop,ro -t iso9660 /media/8890AEDF90AED2CC/DOWNLOAD/livedvd-x86-amd64-32ul-2012.iso /mnt/cdrom
 
 sudo mkdir -p /mnt/usb
 sudo mount -t vfat /dev/sdb1 /mnt/usb
@@ -7,11 +7,11 @@ sudo cp -r /mnt/cdrom/* /mnt/usb
 sudo mv /mnt/usb/isolinux/* /mnt/usb
 sudo mv /mnt/usb/isolinux.cfg /mnt/usb/syslinux.cfg
 sudo rm -rf /mnt/usb/isolinux*
-sudo mv /mnt/usb/memtest86 /mnt/usb/memtest
+#sudo mv /mnt/usb/memtest86 /mnt/usb/memtest
 
 sudo umount /mnt/cdrom
 sudo sed -i -e "s:cdroot:cdroot slowusb:" \
-    -e "s:kernel memtest86:kernel memtest:" \
+#    -e "s:kernel memtest86:kernel memtest:" \
     /mnt/usb/syslinux.cfg
 sudo umount /mnt/usb/
 sudo syslinux /dev/sdb1
