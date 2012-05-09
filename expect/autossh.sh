25dea683f4e6b32c4c6bbff3eb91e501e19d07dc
whille <Whille_Wang@cn.dlink.com>
2012-05-09 17:30:57 +0800

    update

--- expect/autossh.sh
+++ expect/autossh.sh
@@ -10,14 +10,14 @@ set timeout 30
 #log_user 0
 
 spawn scp $home/.ssh/id_rsa.pub $user@$host:
-expect {
-    "yes/no" { 
-        send "yes\r"
-        exp_continue
-    } "password:" { 
-        send "$password\r" 
-    }
-}
+#expect {
+#    "yes/no" { 
+#        send "yes\r"
+#        exp_continue
+#    } "password:" { 
+#        send "$password\r" 
+#    }
+#}
 #interact
 spawn ssh $user@$host
 expect {
@@ -25,9 +25,7 @@ expect {
         send "$password\r" 
         expect "Last login"
         send "\[ ! -d .ssh \] && mkdir -p .ssh && chmod 700 .ssh\r"
-        expect "*#"
         send "cat id_rsa.pub >>.ssh/authorized_keys && chmod 644 .ssh/authorized_keys\r"
-        expect "*#"
         send "rm -f id_rsa.pub\r"
         send "exit\r"
     } "*#" {
