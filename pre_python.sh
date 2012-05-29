1f91679c822629af14280e1d57a61c95bfcf7ff5
whille <Whille_Wang@cn.dlink.com>
2012-05-29 17:30:13 +0800

    update

--- pre_python.sh
+++ pre_python.sh
@@ -1,20 +1,17 @@
 pS  setuptools python2-virtualenv pychecker
-<<<<<<< HEAD
-=======
-easy_install-2.7 -U distribute
-easy_install-2.7 pycassa python-memcached pyramid pexpect
->>>>>>> 3148e613b51eb2444dad9199b8fb15b6f25fbf19
 
 mkdir ~/my_env
 cd ~/my_env/
 virtualenv2 --no-site-packages ~/my_env
 . ~/my_env/bin/activate
 
-easy_install distribute
-easy_install -U pycassa python-memcached pyramid w3lib  w3lib Scrapy
+easy_install -U distribute
+easy_install -U pycassa pyramid pexpect w3lib Scrapy pyrex
 pip install http://projects.unbit.it/downloads/uwsgi-lts.tar.gz
 
 cd mydlink_v3/
 ../bin/python setup.py develop
 
+
+#
 deactivate
