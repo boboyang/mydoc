1bec014bb7f64b43ff94644302d1fbcb9bbcacd9
whille <Whille_Wang@cn.dlink.com>
2012-06-26 17:30:07 +0800

    update

--- pre_python.sh
+++ pre_python.sh
@@ -1,7 +1,8 @@
 pS  setuptools python2-virtualenv pychecker
 
-pip install pycassa pyramid pexpect w3lib Scrapy pyrex numpy matplotlib ipython
+pip install pycassa pyramid pexpect w3lib pysqlite Scrapy pyrex numpy matplotlib ipython
 pip install http://projects.unbit.it/downloads/uwsgi-lts.tar.gz
+
 
 cd mydlink_v3/
 ../bin/python setup.py develop
