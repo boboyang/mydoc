8ebdef98e2394b9c9dc40e7aa6f3622e8c765843
whille <Whille_Wang@cn.dlink.com>
2012-08-11 17:30:09 +0800

    update

--- Pyramid.mm
+++ Pyramid.mm
@@ -3,66 +3,6 @@
 <node COLOR="#000000" CREATED="1322875986124" ID="ID_1998931582" MODIFIED="1344588742356" STYLE="fork" TEXT="Pyramid">
 <font NAME="Dialog" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1322882448138" ID="ID_1767880717" MODIFIED="1322882452953" POSITION="right" TEXT="Views">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="Dialog" SIZE="18"/>
-</node>
-<node COLOR="#0033ff" CREATED="1322882453610" ID="ID_247035930" MODIFIED="1322882461112" POSITION="right" TEXT="Renderers">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="Dialog" SIZE="18"/>
-</node>
-<node COLOR="#0033ff" CREATED="1322882464730" ID="ID_368236607" MODIFIED="1322882466665" POSITION="right" TEXT="Templates">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="Dialog" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1322963468818" ID="ID_1506208029" LINK="Jinja2.mm" MODIFIED="1322963468831" TEXT="Jinja2">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="Dialog" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1322965611858" ID="ID_355461712" MODIFIED="1322968227798" TEXT="pyramid_jinja2">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="Dialog" SIZE="16"/>
-<node COLOR="#990000" CREATED="1322965617074" ID="ID_1725411703" MODIFIED="1338277568956" TEXT="setup">
-<font NAME="Dialog" SIZE="14"/>
-<node COLOR="#111111" CREATED="1322965633154" ID="ID_151215789" MODIFIED="1322965636530" TEXT="development.ini">
-<font NAME="Dialog" SIZE="12"/>
-<node COLOR="#111111" CREATED="1322965637330" ID="ID_241017089" MODIFIED="1322965638178" TEXT="config.include(&apos;pyramid_jinja2&apos;)">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-<node COLOR="#111111" CREATED="1322965684277" ID="ID_1950435961" MODIFIED="1322965685215" TEXT="jinja2.directories = yourapp:templates">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-</node>
-</node>
-<node COLOR="#990000" CREATED="1322965785936" ID="ID_385569467" MODIFIED="1338277568957" TEXT="Usage">
-<font NAME="Dialog" SIZE="14"/>
-<node COLOR="#111111" CREATED="1322966064078" ID="ID_1810758988" MODIFIED="1322966065251" TEXT="Template Lookups">
-<font NAME="Dialog" SIZE="12"/>
-<node COLOR="#111111" CREATED="1322967437704" ID="ID_1016129280" MODIFIED="1322967438523" TEXT="Template Inheritance">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-</node>
-</node>
-<node COLOR="#990000" CREATED="1322968229905" ID="ID_1928243791" MODIFIED="1338277568957" TEXT="Settings">
-<font NAME="Dialog" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1322968246389" ID="ID_1100184591" MODIFIED="1338277568957" TEXT="Filters">
-<font NAME="Dialog" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1322968251429" ID="ID_494610817" MODIFIED="1338277568958" TEXT="create a project">
-<font NAME="Dialog" SIZE="14"/>
-<node COLOR="#111111" CREATED="1322969786522" ID="ID_1018638522" MODIFIED="1322969787429" TEXT="paster create -t pyramid_jinja2_starter">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-</node>
-<node COLOR="#990000" CREATED="1322968264789" ID="ID_773959945" MODIFIED="1338277568958" TEXT="more info">
-<font NAME="Dialog" SIZE="14"/>
-</node>
-</node>
-</node>
-<node COLOR="#0033ff" CREATED="1322882473323" ID="ID_1642574548" MODIFIED="1322882475152" POSITION="right" TEXT="View Configuration">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="Dialog" SIZE="18"/>
-</node>
 <node COLOR="#0033ff" CREATED="1322882507852" ID="ID_1446914574" MODIFIED="1322882508443" POSITION="right" TEXT="Static Assets">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="Dialog" SIZE="18"/>
