414b50e7fe249815b689824407549d2b8d78f01a
whille <Whille_Wang@cn.dlink.com>
2012-08-11 12:53:07 +0800

    update

--- Pyramid.mm
+++ Pyramid.mm
@@ -3,90 +3,6 @@
 <node COLOR="#000000" CREATED="1322875986124" ID="ID_1998931582" MODIFIED="1344588742356" STYLE="fork" TEXT="Pyramid">
 <font NAME="Dialog" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1322879296870" ID="ID_836576627" MODIFIED="1322882812733" POSITION="left" TEXT="create app">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="Dialog" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1322879086816" ID="ID_1524814856" MODIFIED="1338279440440" TEXT="The Project Structure">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="Dialog" SIZE="16"/>
-<node COLOR="#990000" CREATED="1322879131681" ID="ID_345474556" MODIFIED="1338277568949" TEXT="development.ini">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="Dialog" SIZE="14"/>
-<node COLOR="#111111" CREATED="1322879596736" ID="ID_1974284822" MODIFIED="1322880347925" TEXT="db">
-<font NAME="Dialog" SIZE="12"/>
-<node COLOR="#111111" CREATED="1322879820412" ID="ID_100155416" MODIFIED="1322880347930" TEXT="mysql">
-<font NAME="Dialog" SIZE="12"/>
-<node COLOR="#111111" CREATED="1322879592004" ID="ID_1970574107" MODIFIED="1322880347932" TEXT="sqlalchemy.localdb.url">
-<font NAME="Dialog" SIZE="12"/>
-<node COLOR="#111111" CREATED="1322879756926" ID="ID_1357595849" MODIFIED="1322880347935" TEXT="mysql://root@127.0.0.1/csp_development">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-</node>
-<node COLOR="#111111" CREATED="1322879742360" ID="ID_118530462" MODIFIED="1322880347938" TEXT="sqlalchemy.globaldb.url">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-</node>
-<node COLOR="#111111" CREATED="1322879824535" ID="ID_1012877226" MODIFIED="1322880347939" TEXT="sqlite">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-</node>
-<node COLOR="#111111" CREATED="1322879850927" ID="ID_1540596907" MODIFIED="1322880347940" TEXT="redis">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-<node COLOR="#111111" CREATED="1322879852456" ID="ID_1459687644" MODIFIED="1322880347952" TEXT="cspservice">
-<font NAME="Dialog" SIZE="12"/>
-<node COLOR="#111111" CREATED="1322879886781" ID="ID_1622379869" MODIFIED="1322880347953" TEXT="host">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-<node COLOR="#111111" CREATED="1322879888793" ID="ID_971691626" MODIFIED="1322880347955" TEXT="port">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-</node>
-<node COLOR="#111111" CREATED="1322879862392" ID="ID_1964795106" MODIFIED="1322880347956" TEXT="jinja2">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-</node>
-<node COLOR="#990000" CREATED="1322879133634" ID="ID_1357015410" MODIFIED="1338277568952" TEXT="product.ini">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="Dialog" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1322879105680" ID="ID_1683606021" MODIFIED="1338277568952" TEXT="setup.py">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="Dialog" SIZE="14"/>
-<node COLOR="#111111" CREATED="1322879922623" ID="ID_727095111" MODIFIED="1322880347961" TEXT="requires">
-<font NAME="Dialog" SIZE="12"/>
-<node COLOR="#111111" CREATED="1322879949291" ID="ID_1851072004" MODIFIED="1322880347962" TEXT="     &apos;redis&apos;,&#xa;    &apos;pyramid_mailer&apos;,&#xa;    &apos;coverage&apos;,&#xa;    &apos;MySQL-python&apos;,&#xa;    &apos;thrift&apos;,&#xa;    &apos;pyramid_jinja2&apos;,">
-<font NAME="Dialog" SIZE="12"/>
-</node>
-</node>
-</node>
-<node COLOR="#990000" CREATED="1322879104800" ID="ID_933281758" MODIFIED="1338277568953" TEXT="setup.cfg">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="Dialog" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1322879155218" ID="ID_1467760149" MODIFIED="1338277568953" TEXT="project package">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="Dialog" SIZE="14"/>
-</node>
-</node>
-</node>
-<node COLOR="#0033ff" CREATED="1322882443370" ID="ID_1721617353" MODIFIED="1322882444569" POSITION="right" TEXT="URL Dispatch">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="Dialog" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1322886139682" ID="ID_913706982" MODIFIED="1322886160382" TEXT="config.">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="Dialog" SIZE="16"/>
-<node COLOR="#990000" CREATED="1322886141182" ID="ID_1688985538" MODIFIED="1338277568954" TEXT="add_route">
-<font NAME="Dialog" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1322886144014" ID="ID_882430719" MODIFIED="1338277568954" TEXT="add_view">
-<font NAME="Dialog" SIZE="14"/>
-</node>
-<node COLOR="#990000" CREATED="1322886148511" ID="ID_1521178242" MODIFIED="1338277568954" TEXT="scan">
-<font NAME="Dialog" SIZE="14"/>
-</node>
-</node>
-</node>
 <node COLOR="#0033ff" CREATED="1322882448138" ID="ID_1767880717" MODIFIED="1322882452953" POSITION="right" TEXT="Views">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="Dialog" SIZE="18"/>
@@ -105,9 +21,6 @@
 <node COLOR="#00b439" CREATED="1322965611858" ID="ID_355461712" MODIFIED="1322968227798" TEXT="pyramid_jinja2">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="Dialog" SIZE="16"/>
-<node COLOR="#990000" CREATED="1322964699093" ID="ID_718170453" MODIFIED="1338277568956" TEXT="sudo easy_install pyramid_jinja2">
-<font NAME="Dialog" SIZE="14"/>
-</node>
 <node COLOR="#990000" CREATED="1322965617074" ID="ID_1725411703" MODIFIED="1338277568956" TEXT="setup">
 <font NAME="Dialog" SIZE="14"/>
 <node COLOR="#111111" CREATED="1322965633154" ID="ID_151215789" MODIFIED="1322965636530" TEXT="development.ini">
