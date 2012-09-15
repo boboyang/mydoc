7e56ed9905583ca78fc0f1ccf31a0a7ea16ced02
whille <Whille_Wang@cn.dlink.com>
2012-09-15 08:33:58 +0800

    update

--- expect/bj2.sh
+++ expect/bj2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 for i in 61 62 63 64 66 65 216 
 do
-    python2 autossh.py 172.18.195.$i root bsdc707 
+    python2 autossh.py 172.18.195.$i root $bjrdpasswd
 done
--- linux/archlinux.sh
+++ linux/archlinux.sh
@@ -46,7 +46,7 @@ history -w tmp.txt
 #reboot
 
 # prepare wlan0
-wpa_passphrase mydlink01 dlink930 > /etc/wpa_supplicant.conf
+wpa_passphrase [router] [key] > /etc/wpa_supplicant.conf
 wpa_supplicant -B -Dwext -i wlan0 -c /etc/wpa_supplicant.conf
 dhcpcd wlan0
 #test: ping www.baidu.com
@@ -55,9 +55,6 @@ dhcpcd wlan0
 pacman -S filesystem --force
 rm /etc/profile.d/locale.sh 
 pacman -Syu
-
-#speed up 
-pacman -S aria2
 
 vi /etc/pacman.conf
 #add
@@ -72,23 +69,22 @@ pacman -Syy
 # run `grep "pacman -S vim" pre_archlinux.sh`
 
 #desktop
-pacman -S wicd wicd-gtk rp-pppoe xorg xfce4 slim zlib sudo consolekit ibus ibus-sunpinyin wqy-bitmapfont wqy-zenhei openntpd gamin dbus tree abs xlockmore xarchiver thunar-archive-plugin thunar-volman ntfs-3g
+pacman -S wicd wicd-gtk rp-pppoe xorg xfce4 slim unrar zlib sudo consolekit ibus ibus-sunpinyin openntpd gamin dbus tree abs xlockmore thunar-volman ntfs-3g
 #power
-pacman -S cpufrequtils acpid xfce4-power-manager xfce4-battery-plugin
 #office
-pacman -S freemind stardict feh scrot galculator gedit gedit-plugins mupdf vim 
+pacman -S aspell-en enchant freemind goldendict feh galculator gedit gedit-plugins txt2tags mupdf vim xfce4-screenshooter chmsee thunar-vfs-1 libreoffice-{common,base,calc,draw,impress,math,writer,gnome}
+
 #internet
-pacman -S skype chromium flashplugin icedtea-web thunderbird openssh
+pacman -S aria2 skype chromium flashplugin icedtea-web thunderbird openssh bin32-wine 
 #multimedia
-pacman -S pulseaudio alsa-utils gstreamer0.10-plugins smplayer audacious gvfs-afc 
+pacman -S pulseaudio alsa-utils gstreamer0.10-plugins smplayer audacious mcomix
+
+yaourt -S wine-thunder wqy-microhei briss qterm osdlyrics aliedit libXinerama libXft bcompare 
+
 #dev
-pacman -S base-devel cmake ccache wireshark-gtk ethtool sysstat gdb curl git   
+pacman -S base-devel cmake ccache wireshark-gtk ethtool sysstat gdb curl git gvfs-afc ctags cscope memcached redis libmemcached tk mysql 
 
-yaourt -S briss
-
-sed -i 's/^MODULES=()/^MODULES=(acpi-cpufreq cpufreq_ondemand cpufreq_powersave)/g' /etc/rc.conf 
-#
-#DAEMONS=(hwclock syslog-ng acpid dbus @cpufreq slim wicd crond)
+#DAEMONS=(hwclock syslog-ng acpid dbus slim wicd crond)
 
 #power management
 vi /boot/syslinux/syslinux.cfg
@@ -105,7 +101,7 @@ sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 adduser
 #Login name for new user []: whille
-#Additional groups (comma separated) []: audio,lp,optical,storage,video,wheel,games,power,scanner
+#Additional groups (comma separated) []: audio,lp,optical,storage,video,wheel,games,power,scanner,users
 
 # visudo
 # uncomment wheel to enable sudo users
@@ -116,61 +112,11 @@ cp /etc/skel/.xinitrc ~/
 chown whille ~/.xinitrc
 echo "exec ck-launch-session dbus-launch --exit-with-session startxfce4" >> ~/.xinitrc
 
-#alias
-# ~/.bashrc
-alias la='ls -a --color=auto'
-alias ll='ls -aoth --color=auto'
-alias ls='ls --color=auto'
-alias pQs='sudo pacman -Qs'
-alias pS='sudo pacman -S'
-alias pU='sudo pacman -U'
-alias txC='iconv -f gb2312 -c -t utf-8'
-
 # set input method in ibus
-
-git config --global user.email "whille@163.com"
-git config --global user.name "whille"
-
-#qterm
-cd ~/abs/
-tar zxvf qterm.tar.gz 
-cd qterm
-makepkg -s
-pU qterm-[TAB]
-
-#osdlyrics
-cd ~/abs/
-tar zxvf osdlyrics.tar && cd osdlyrics
-makepkg -s
-pU osdlyrics-0.4.2-2-i686.pkg.tar.xz
 
 #ssh setting
 ssh-keygen -C 'whille@163.com' -t rsa
 
 # develop
 echo /usr/local/lib > /etc/ld.so.conf.d/local.conf 
-
-#hot key
-#Alt + F2: run
-#Alt + F8:resize window
-#Alt + F9:min window
-#Alt + F10:max window
-
-#xflock4: lock screen
-#sudo pm-hibernate
-#sudo pm-suspend
-#Ctrl + T: open new tab
-#Ctrl + W: close tab
-#Ctrl + Page up/Page down
-#Ctrl + L: clear term text
-
-# sshd,  root
-#rc.d {start|stop|restart} sshd
-# ssh client
-#$ ssh -p port user@server-address
-
-#xunlei
-pS bin32-wine 
-yaourt -S wine-thunder
-
 
